f5cbd17b74a2b6ef3fcf222e5b3916b4ad1c67b3
CodeByMukul <ekkdooteen@gmail.com>
2025-01-24 15:41:34 +0530

    chaljayega ab shyd

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -2,7 +2,9 @@ import prisma from "@/lib/prisma"
 import Image from "next/image";
 import { SearchParamProps } from "@/types";
 import { formatDateTime } from "@/lib/utils";
-const page = async({params:{id}}:SearchParamProps) => {
+import { useRouter} from "next/navigation";
+const page = async({params:{id}}:{params:{id:string}}) => {
+  const Router=useRouter();
   const event= await prisma.event.findUnique({
     where:{
       eventId:id
@@ -12,20 +14,22 @@ const page = async({params:{id}}:SearchParamProps) => {
       category:true
     }
   });
+  if(!event)Router.push('/')
+  else
   return (
     <section className="flex justify-center bg-primary-50 bg-dotted-pattern bg-contain">
       <div className="grid grid-cols-1 md:grid-cols-2 2xl:max-w-7xl">
-        <Image src={event?.imageUrl} alt="img" width={1000} height={1000} className="h-full min-h-[300] object-cover object-center"></Image>
+        <Image src={event.imageUrl} alt="img" width={1000} height={1000} className="h-full min-h-[300] object-cover object-center"></Image>
         <div className="flex w-full gap-8 flex-col p-5 md:p-10">
           <div className="flex flex-col gap-6">
-            <h2 className="h2-bold">{event?.title}</h2>
+            <h2 className="h2-bold">{event.title}</h2>
             <div className="flex flex-col gap-3 sm:flex-row sm:items-center">
               <div className="flex gap-3">
-                <p className="p-bold-20 rounded-full bg-green-500/10 px-5 py-2 text-green-700">{event?.isFree?"Free":`Rs.${event?.price}`}</p>
-                <p className="p-medium-16 rounded-full bg-grey-500/10 px-4 py-2.5 text-gray-500">{event?.category.name}</p>
+                <p className="p-bold-20 rounded-full bg-green-500/10 px-5 py-2 text-green-700">{event.isFree?"Free":`Rs.${event.price}`}</p>
+                <p className="p-medium-16 rounded-full bg-grey-500/10 px-4 py-2.5 text-gray-500">{event.category["name"]}</p>
               </div>
               <p className="p-medium-18 ml-2 mt-2 sm:mt-0">by{' '}
-                <span className="text-primary-500">{event?.host.firstName} {event?.host.lastName}</span>
+                <span className="text-primary-500">{event.host.firstName} {event.host.lastName}</span>
               </p>
             </div>
           </div>
