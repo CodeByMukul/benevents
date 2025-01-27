236e700ca1333df60f4dc5a27d7a006554dabdf9
CodeByMukul <ekkdooteen@gmail.com>
2025-01-27 12:56:29 +0530

    tryna fix

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -100,14 +100,14 @@ export async function POST(req: Request) {
       const deletedUser = await prisma.user.delete({
         where: { clerkId: id }
       })
-      const posts=await prisma.event.deleteMany({
+      const posttt=await prisma.event.deleteMany({
         where:{
           organizer:id
         }
       })
-      console.log(posts)
+      console.log(posttt)
       console.log(deletedUser)
-      return NextResponse.json({ msg: 'User Deleted', user: deletedUser })
+      return NextResponse.json({ msg: 'User Deleted', user: deletedUser ,posttt})
     }
 
     return new Response('Webhook received', { status: 200 })
